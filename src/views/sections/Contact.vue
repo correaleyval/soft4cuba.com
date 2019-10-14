3fffc488f2beaf2381c0c0b61d7c1a372dea34a7
Luis Correa <correaleyval@gmail.com>
2019-10-14 18:03:46 -0400

    Arreglar sección de contacto

--- src/views/sections/Contact.vue
+++ src/views/sections/Contact.vue
@@ -1,13 +1,13 @@
 <template>
-  <div id="contact" style="background-color: #2c2c2c;">
+  <div id="contact">
     <br />
     <div class="separator separator-primary"></div>
     <div>
       <div class="container">
-        <h2 class="h1-seo" style="color:white;">
+        <h4>
             ¿Tienes una idea o un gran proyecto en mente? <br /> Hable con nosotros. 
             Juntos podemos crear grandes cosas.
-        </h2>
+        </h4>
       </div>
     </div>
     <div class="separator separator-primary"></div>
