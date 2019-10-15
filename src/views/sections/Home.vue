42c28b2e0a34e1d5ba601bf4453751cb3ea3ab11
Luis Correa <correaleyval@gmail.com>
2019-10-15 09:34:09 -0400

    Agregado up button

--- src/views/sections/Home.vue
+++ src/views/sections/Home.vue
@@ -3,23 +3,36 @@
     <parallax class="page-header-image" style="background-image:url('images/hero-bg.jpg')"></parallax>
     <div class="container">
       <div class="content-center brand">
-        <img class="logo" src="@/assets/logo.png" data-aos="fade-up"/>
-        <h1 class="h1-seo" data-aos="zoom-in-down">Soluciones creativas para convertir tu idea en una realidad</h1>
+        <img class="logo" src="@/assets/logo.png" data-aos="fade-up" />
+        <h1
+          class="h1-seo"
+          data-aos="zoom-in-down"
+        >Soluciones creativas para convertir tu idea en una realidad</h1>
         <div class="text-center">
-          <a data-aos="zoom-in"
-            href="https://t.me/soft4cuba"
-            class="btn btn-primary btn-icon btn-round btn-lg mr-5"
-            target="_blank"
-          >
-            <i class="fab fa-telegram"></i>
-          </a>
-          <a data-aos="zoom-in"
+          <div class="row"></div>
+          <a
+            data-aos="zoom-in"
             href="https://www.facebook.com/groups/495795397671271/"
-            class="btn btn-primary btn-icon btn-round btn-lg ml-5"
+            class="btn btn-primary btn-icon btn-round btn-lg mr-4"
             target="_blank"
           >
             <i class="fab fa-facebook-square"></i>
           </a>
+          <a
+            data-aos="zoom-in"
+            href="https://t.me/soft4cuba"
+            class="btn btn-primary btn-icon btn-round btn-lg mr-4"
+            target="_blank"
+          >
+            <i class="fab fa-telegram"></i>
+          </a>
+          <router-link
+            data-aos="zoom-in"
+            to="/proyects"
+            class="btn btn-primary btn-icon btn-round btn-lg"
+          >
+            <i class="now-ui-icons design_app"></i>
+          </router-link>
         </div>
       </div>
     </div>
