38f439e98e067bd7f7c60b2a7667aede431469e8
Luis Correa <correaleyval@gmail.com>
2019-10-14 21:23:32 -0400

    Creada pagina para mostrar proyectos

---
+++ src/layout/ProyectNavbar.vue
@@ -0,0 +1,63 @@
+<template>
+  <navbar
+    position="fixed"
+    type="dark"
+    :transparent="transparent"
+    :color-on-scroll="colorOnScroll"
+    menu-classes="ml-auto"
+  >
+    <template>
+      <router-link v-popover:popover1 class="navbar-brand" to="/">
+        <img src="@/assets/logo.png" />
+      </router-link>
+    </template>
+    
+    <template slot="navbar-menu">
+      <li class="nav-item" @click="navlinkClick">
+        <router-link class="nav-link" to="/">
+          <i class="now-ui-icons media-1_button-play"></i>
+          <p>Inicio</p>
+        </router-link>
+      </li>
+      <li class="nav-item" @click="navlinkClick">
+        <router-link class="nav-link" to="/proyects">
+          <i class="now-ui-icons design_app"></i>
+          <p>Proyectos</p>
+        </router-link>
+      </li>
+    </template>
+  </navbar>
+</template>
+
+<script>
+
+import { Navbar } from "@/components";
+import { Popover } from "element-ui";
+
+import EventBus from '@/EventBus'
+
+export default {
+  name: "proyect-navbar",
+  props: {
+    transparent: Boolean,
+    colorOnScroll: Number
+  },
+  components: {
+    Navbar,
+    [Popover.name]: Popover
+  },
+  methods: {
+    navlinkClick() {
+      EventBus.$emit('close-navbar')
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar-brand {
+  img {
+    height: 32px;
+  }
+}
+</style>
---
+++ src/views/Proyects.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="section section-nucleo-icons"
+    style="background-image: url('img/bg11.jpg'); background-size: cover; background-position: top center; min-height: 700px;"
+  >
+    <div class="container">
+      <card class="mt-5">
+        <div class="row">
+          <div class="col-lg-6 col-md-12">
+            <img v-lazy="'img/page.png'" alt="Raised Image" class="rounded img-raised" />
+          </div>
+          <div class="col-lg-6 col-md-12">
+            <h2 class="title">Soft4Cuba.com</h2>
+            <h5>Web principal de nuestra comunidad</h5>
+            <!-- <div class="nucleo-container">
+                    <img v-lazy="'assetsimg/nucleo.svg'" alt="">
+            </div>-->
+            <a
+              href="https://github.com/correaleyval/soft4cuba.com"
+              class="btn btn-primary btn-round btn-lg"
+              target="_blank"
+            >
+              <i class="fab fa-github"></i> Ver código en Github
+            </a>
+          </div>
+        </div>
+      </card>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Card } from "@/components";
+
+export default {
+  components: {
+    Card
+  }
+};
+</script>
