9a67fac981f5cd38d25db21999cdadd5aec89d1a
Luis Correa <correaleyval@gmail.com>
2019-10-15 09:32:40 -0400

    Agregadis estilos para ocultar elementos según la resolución

--- src/App.vue
+++ src/App.vue
@@ -15,4 +15,26 @@ export default {};
 .sidebar-collapse .navbar-collapse:before {
   background: linear-gradient(#212529 0%, #000 80%) !important;
 }
+
+@media (max-width: 767px) {
+  .hidden-xs {
+    display: none !important;
+  }
+}
+@media (min-width: 768px) and (max-width: 991px) {
+  .hidden-sm {
+    display: none !important;
+  }
+}
+@media (min-width: 992px) and (max-width: 1199px) {
+  .hidden-md {
+    display: none !important;
+  }
+}
+@media (min-width: 1200px) {
+  .hidden-lg {
+    display: none !important;
+  }
+}
+
 </style>
