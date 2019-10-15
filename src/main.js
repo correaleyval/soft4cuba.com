a23bf18e42cd81d9ed05ce1fc0942b579affb35d
Luis Correa <correaleyval@gmail.com>
2019-10-14 22:20:30 -0400

    Instalado y configurado AOS para las animaciones

--- src/main.js
+++ src/main.js
@@ -6,11 +6,23 @@ import './registerServiceWorker'
 
 import NowUiKit from './plugins/now-ui-kit';
 
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
 Vue.config.productionTip = false
 
 Vue.use(NowUiKit);
 
 new Vue({
+  created() {
+    AOS.init({
+      offset: 120,
+      duration: 600,
+      easing: 'ease-in-sine',
+      delay: 300,
+      once: true,
+    })
+  },
   router,
   store,
   render: h => h(App)
