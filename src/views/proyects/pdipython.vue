e4a7b65550f4603ac583aeb7bfe971db9c392676
Luis Correa <correaleyval@gmail.com>
2019-11-01 12:11:03 -0400

    Update pdipython.vue

--- src/views/proyects/pdipython.vue
+++ src/views/proyects/pdipython.vue
@@ -8,27 +8,24 @@
         <br />
         <h2 class="h2-seo">PDI Python</h2>
         <h5>Curso de procesamiento de imágenes con Python</h5>
-
-        <div class="row">
+        <div class="col-md-6">
+            <a
+              href="https://nbviewer.jupyter.org/github/correaleyval/Procesamiento-de-Imagenes/tree/master"
+              class="btn btn-primary btn-round"
+              target="_blank"
+            >
+              Leer Online
+            </a>
+          </div>
           <div class="col-md-6">
             <a
               href="https://github.com/correaleyval/Procesamiento-de-Imagenes"
-              class="btn btn-primary btn-round btn-md"
+              class="btn btn-primary btn-round"
               target="_blank"
             >
               <i class="fab fa-github"></i> Descargar de Github
             </a>
           </div>
-          <div class="col-md-6">
-            <a
-              href="https://github.com/correaleyval/Procesamiento-de-Imagenes"
-              class="btn btn-primary btn-round btn-md"
-              target="_blank"
-            >
-              <i class="fab fa-github"></i> Leer Online
-            </a>
-          </div>
-        </div>
       </div>
     </div>
   </card>
